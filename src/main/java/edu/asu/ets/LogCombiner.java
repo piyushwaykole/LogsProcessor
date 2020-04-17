8baa4f961c9b254193ade524152c3f7c36ea28d4
piyushwaykole <piyushwaykole@gmail.com>
2020-04-16 17:07:24 -0700

    Code cleaning and duplicate removal changes

--- src/main/java/edu/asu/ets/LogCombiner.java
+++ src/main/java/edu/asu/ets/LogCombiner.java
@@ -4,215 +4,84 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LogCombiner {
 
-	public static final String LICENSE_FILE_PREFIX = "license.log";
-	public static final String TIMESTAMP = "TIMESTAMP";
+	public static final String DELIMITER = "/";
 
-	public static void main(String[] args) {
+	public static void combine(String args) {
 
 		LogCombiner logc = new LogCombiner();
-		String[] filenames;
+		String[] allfiles = logc.getAllFiles(args);
+		String destFileName = args + DELIMITER + "combined.log";
 		try {
-			filenames = logc.getLicenseFileNames(args[0]);
+			BufferedWriter writer = new BufferedWriter(new FileWriter(destFileName, true));
+			logc.combine(writer, args, allfiles);
+			writer.close();
 
-			for (String f : filenames) {
-				System.out.println(f);
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage() + " --> Check Folder path and try again");
-			return;
-		}
-
-		for (String f : filenames) {
-
-			String file = args[0] + "\\" + "\\" + f;
-			System.out.println(file);
-			try {
-				Scanner scanner = new Scanner(new File(file));
-				Map<String, StringBuilder> m = logc.createSeperateFiles(scanner);
-				logc.saveMapToFiles(args[0], m);
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		}
-
-	}
-
-	private String[] getLicenseFileNames(String dirPath) throws FileNotFoundException {
-
-		File folderName = new File(dirPath);
-
-		if (folderName.exists() && folderName.isDirectory()) {
-
-			// Filter to only get license files from that folder
-			FilenameFilter licenseFilter = new FilenameFilter() {
-
-				public boolean accept(File dir, String name) {
-					if (name.startsWith(LICENSE_FILE_PREFIX))
-						return true;
-					else
-						return false;
-				}
-			};
-
-			String[] files = folderName.list(licenseFilter);
-			return files;
-		}
-		throw new FileNotFoundException("Either Folder does not exists or is not a directory");
-	}
-
-	private Map<String, StringBuilder> createSeperateFiles(Scanner scanner) {
-
-		Map<String, StringBuilder> map = new HashMap<String, StringBuilder>();
-		StringBuilder unknown = new StringBuilder();
-
-		String firstDateInFile = null;
-		int firstDateCount = 0;
-
-		String currentDate = null;
-
-		String line = "";
-		while (scanner.hasNextLine()) {
-			line = scanner.nextLine();
-
-			if (line.contains(TIMESTAMP))
-				break;
-
-			unknown.append(line);
-			unknown.append("\n");
-
-		}
-
-		firstDateInFile = line.split(" ")[3];
-		firstDateCount++;
-
-		currentDate = firstDateInFile;
-		StringBuilder curr = new StringBuilder();
-
-		while (scanner.hasNextLine()) {
-			line = scanner.nextLine();
-
-			if (line.contains(TIMESTAMP)) {
-				String[] splitString = line.split(" ");
-
-				if (!currentDate.equals(splitString[3])) {
-					map.put(currentDate, curr);
-					curr = new StringBuilder();
-					currentDate = splitString[3];
-				}
-
-				if (currentDate.equals(firstDateInFile))
-					firstDateCount++;
-			}
-
-			curr.append(line);
-			curr.append("\n");
-
-		}
-
-		map.put(currentDate, curr);
-
-		if (firstDateCount < 8) {
-			unknown.append(map.get(firstDateInFile).toString());
-			map.put(firstDateInFile, unknown);
-		}
-
-		String prevDate = getPreviousDate(firstDateInFile);
-		if (prevDate != null)
-			map.put(prevDate, unknown);
-
-		return map;
-	}
-
-	private String getPreviousDate(String date) {
-		SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
-		Date day;
-		try {
-			day = formatter.parse(date);
-		} catch (ParseException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return null;
 		}
-		Calendar calender = Calendar.getInstance();
-		calender.setTime(day);
-
-		calender.add(Calendar.DAY_OF_YEAR, -1);
-
-		Date prevDate = calender.getTime();
-		String result = formatter.format(prevDate);
-
-		return result;
-
 	}
 
-	private String getFileName(String date) {
-
-		StringBuilder st = new StringBuilder();
-
-		for (int i = 0; i < date.length(); i++) {
-			if (date.charAt(i) == '/')
-				st.append('.');
-			else
-				st.append(date.charAt(i));
+	private String[] getAllFiles(String dirPath) {
+		File files = new File(dirPath);
+		String[] allfiles = null;
+		if (files.exists() && files.isDirectory()) {
+			allfiles = files.list();
 		}
 
-		return st.toString();
-	}
+		Arrays.sort(allfiles, (a, b) -> {
 
-	private void saveMapToFiles(String dirPath, Map<String, StringBuilder> map) throws IOException {
+			String[] splita = a.split("\\.");
+			String[] splitb = b.split("\\.");
 
-		for (Map.Entry<String, StringBuilder> entry : map.entrySet()) {
-			String fileName = dirPath + "\\" + "\\" + getFileName(entry.getKey());
+			if (splita[2].compareTo(splitb[2]) == 0) {
 
-			File file = new File(fileName);
-			BufferedWriter writer = null;
-			if (file.exists()) {
-				try {
-					writer = new BufferedWriter(new FileWriter(fileName, true));
-					writer.append(entry.getValue());
-					writer.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} finally {
-					if (writer != null)
-						writer.close();
+				int montha = Integer.parseInt(splita[0]);
+				int monthb = Integer.parseInt(splitb[0]);
+				if (montha == monthb) {
+					int daya = Integer.parseInt(splita[1]);
+					int dayb = Integer.parseInt(splitb[1]);
+					return daya - dayb;
 				}
+				return montha - monthb;
+			}
+			return splita[2].compareTo(splitb[2]);
+		});
+		System.out.println(Arrays.toString(allfiles));
+		return allfiles;
+	}
 
-			} else {
+	private void combine(BufferedWriter handle, String dirpath, String[] allfiles) {
+
+		for (String file : allfiles) {
+			String path = dirpath + DELIMITER + file;
+
+			File filehandle = new File(path);
+			if (filehandle.exists()) {
 				try {
-					writer = new BufferedWriter(new FileWriter(fileName));
-					writer.write(entry.getValue().toString());
-					writer.close();
+					Scanner scanner = new Scanner(filehandle);
+					handle.append("-------------------------------" + file + "-------------------------------" + "\n");
+					while (scanner.hasNextLine()) {
+						handle.append(scanner.nextLine());
+						handle.append("\n");
+					}
+					scanner.close();
+					handle.newLine();
+
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-
-				} finally {
-					if (writer != null)
-						writer.close();
 				}
 			}
-
 		}
-
 	}
-
 }
---
+++ src/main/java/edu/asu/ets/LogDivider.java
@@ -0,0 +1,250 @@
+package edu.asu.ets;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class LogDivider {
+
+	public static final String LICENSE_FILE_ANSYS = "license.log";
+	public static final String LICENSE_FILE_ABAQUS = "abaquslm.log";
+	public static final String TIMESTAMP = "TIMESTAMP";
+	public static final String DELIMITER = "/";
+
+	private String firstInEveryFile = "";
+	private String rootPath;
+
+	public LogDivider(String path) {
+		this.rootPath = path;
+	}
+
+	public static void main(String[] args) {
+
+		String rootpath = args[0];
+		if (args[0].charAt(args[0].length() - 1) == '/')
+			rootpath = args[0].substring(0, args[0].length() - 1);
+
+		LogDivider logc = new LogDivider(rootpath);
+		String[] filenames;
+		try {
+			filenames = logc.getLicenseFileNames(rootpath);
+
+			for (String f : filenames) {
+				System.out.println(f);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage() + " --> Check Folder path and try again");
+			return;
+		}
+
+		// create a separate logs folder to save all individual log file
+		File logFolder = new File(rootpath + DELIMITER + "logs");
+		if (!logFolder.exists()) {
+			logFolder.mkdir();
+		}
+
+		for (String f : filenames) {
+
+			String file = rootpath + DELIMITER + f;
+			System.out.println(file);
+			try {
+				Scanner scanner = new Scanner(new File(file));
+				logc.firstInEveryFile = null;
+				Map<String, StringBuilder> m = logc.createSeperateFiles(scanner);
+				logc.saveMapToFiles(rootpath, m);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		String path = rootpath + DELIMITER + "logs";
+		LogCombiner.combine(path);
+
+	}
+
+	private String[] getLicenseFileNames(String dirPath) throws FileNotFoundException {
+
+		File folderName = new File(dirPath);
+
+		if (folderName.exists() && folderName.isDirectory()) {
+
+			// Filter to only get license files from that folder
+			FilenameFilter licenseFilter = new FilenameFilter() {
+
+				public boolean accept(File dir, String name) {
+					if (name.startsWith(LICENSE_FILE_ANSYS) || name.startsWith(LICENSE_FILE_ABAQUS))
+						return true;
+					else
+						return false;
+				}
+			};
+
+			String[] files = folderName.list(licenseFilter);
+			return files;
+		}
+		throw new FileNotFoundException("Either Folder does not exists or is not a directory");
+	}
+
+	private Map<String, StringBuilder> createSeperateFiles(Scanner scanner) {
+
+		Map<String, StringBuilder> map = new HashMap<String, StringBuilder>();
+		StringBuilder unknown = new StringBuilder();
+
+		String firstDateInFile = null;
+		int firstDateCount = 0;
+
+		String currentDate = null;
+
+		String line = "";
+		while (scanner.hasNextLine()) {
+			line = scanner.nextLine();
+
+			if (line.contains(TIMESTAMP))
+				break;
+
+			unknown.append("\n");
+			unknown.append(line);
+		}
+
+		String[] splt = line.split(" ");
+		firstDateInFile = splt[splt.length - 1];
+		firstDateCount++;
+
+		currentDate = firstDateInFile;
+		StringBuilder curr = new StringBuilder();
+
+		while (scanner.hasNextLine()) {
+			line = scanner.nextLine();
+
+			if (line.contains(TIMESTAMP)) {
+				String[] splitString = line.split(" ");
+
+				String newDate = splitString[splitString.length - 1];
+
+				if (!currentDate.equals(newDate)) {
+					map.put(currentDate, curr);
+					curr = new StringBuilder();
+					currentDate = newDate;
+				}
+
+				if (currentDate.equals(firstDateInFile))
+					firstDateCount++;
+			}
+
+			curr.append(currentDate + " ");
+			curr.append(line);
+			curr.append("\n");
+
+		}
+
+		map.put(currentDate, curr);
+
+		if (firstDateCount < 8) {
+			String val = Pattern.compile("\n").matcher(unknown).replaceAll("\n" + firstDateInFile + " ");
+			unknown = new StringBuilder(val);
+			unknown.append(map.get(firstDateInFile).toString());
+			map.put(firstDateInFile, unknown);
+		} else {
+			String prevDate = getPreviousDate(firstDateInFile);
+			String val = Pattern.compile("\n").matcher(unknown).replaceAll("\n" + prevDate + " ");
+			unknown = new StringBuilder(val);
+
+			if (prevDate != null)
+				map.put(prevDate, unknown);
+
+			this.firstInEveryFile = prevDate;
+
+		}
+
+		return map;
+	}
+
+	private String getPreviousDate(String date) {
+		SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+		Date day;
+		try {
+			day = formatter.parse(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		Calendar calender = Calendar.getInstance();
+		calender.setTime(day);
+
+		calender.add(Calendar.DAY_OF_YEAR, -1);
+
+		Date prevDate = calender.getTime();
+		String result = formatter.format(prevDate);
+
+		return result;
+
+	}
+
+	private String getFileName(String date) {
+
+		StringBuilder st = new StringBuilder();
+
+		for (int i = 0; i < date.length(); i++) {
+			if (date.charAt(i) == '/')
+				st.append('.');
+			else
+				st.append(date.charAt(i));
+		}
+
+		return st.toString();
+	}
+
+	private void saveMapToFiles(String dirPath, Map<String, StringBuilder> map) throws IOException {
+
+		for (Map.Entry<String, StringBuilder> entry : map.entrySet()) {
+			String fileName = dirPath + DELIMITER + "logs" + DELIMITER + getFileName(entry.getKey());
+
+			File file = new File(fileName);
+			BufferedWriter writer = null;
+			if (entry.getKey().equals(this.firstInEveryFile) && file.exists()) {
+				try {
+					writer = new BufferedWriter(new FileWriter(fileName, true));
+					writer.append(entry.getValue());
+					writer.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} finally {
+					if (writer != null)
+						writer.close();
+				}
+
+			} else {
+				try {
+					writer = new BufferedWriter(new FileWriter(fileName));
+					writer.write(entry.getValue().toString());
+					writer.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+
+				} finally {
+					if (writer != null)
+						writer.close();
+				}
+			}
+
+		}
+	}
+}
